11d53920d5b01c274ecb8820d98208130eeeef37
Sorrel Brigman <sorrel.k@gmail.com>
2016-12-12 15:22:47 -0600

    added functionality to the alphabits and palindrome functions

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ console.log("hi")
 //Global var
 //holds the string once it has been turned into an array
 var stringArray = [];
+var reversedString = "";
 
 
 
@@ -14,20 +15,32 @@ function reversal() {
     answer += stringArray[i];
   }
   document.querySelector(".reversal").innerHTML = `<p>Reversal: ${answer}`;
+  reversedString = answer;
 }
 
 function alphabits() {
+  var alphaArray = stringArray.sort();
+  var answer = "";
+  for(var i = 0; i < alphaArray.length; i++){
+    answer += alphaArray[i];
+  }
+  document.querySelector(".alphabits").innerHTML = `<p>Alphabits: ${answer}`;
 
 }
 
 function palindrome() {
+  if (reversedString === document.querySelector("input").value) {
+    document.querySelector(".palindrome").innerHTML = `<p>Your string is a palindrome!</p>`
+  } else {
+    document.querySelector(".palindrome").innerHTML = "";
+      }
 
 }
 
-var testString = "";
-reversal(testString);
-alphabits(testString);
-palindrome(testString);
+// var testString = "";
+// reversal(testString);
+// alphabits(testString);
+// palindrome(testString);
 
 
 var getString = function(e) {
@@ -35,6 +48,8 @@ var getString = function(e) {
   var userString = document.querySelector("input").value;
   stringArray = userString.split("");
   reversal();
+  alphabits();
+  palindrome();
 }
 
 //listener event for submit
