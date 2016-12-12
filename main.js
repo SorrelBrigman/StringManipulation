8a7d631cbc6db9153f99da13f2f0ad56abb69e50
Sorrel Brigman <sorrel.k@gmail.com>
2016-12-12 15:08:04 -0600

    functional reversal function

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 console.log("hi")
 
 
-function reversal() {
+//Global var
+//holds the string once it has been turned into an array
+var stringArray = [];
 
+
+
+//ask if "input should only be letters" is just placeholder, or a function
+function reversal() {
+  var answer = "";
+  for(var i = (stringArray.length-1); i >= 0; i--){
+    answer += stringArray[i];
+  }
+  document.querySelector(".reversal").innerHTML = `<p>Reversal: ${answer}`;
 }
 
 function alphabits() {
@@ -17,3 +28,15 @@ var testString = "";
 reversal(testString);
 alphabits(testString);
 palindrome(testString);
+
+
+var getString = function(e) {
+  e.preventDefault();
+  var userString = document.querySelector("input").value;
+  stringArray = userString.split("");
+  reversal();
+}
+
+//listener event for submit
+
+document.querySelector(".submit").addEventListener("click", getString);
